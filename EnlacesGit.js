c0d62703ac626a9b6839150815a31dd863f484f6
GonzaloPerezCadaveira <gperezmdp@gmail.com>
2022-05-30 18:22:21 -0300

    CicloFor.js

---
+++ EnlacesGit.js
@@ -0,0 +1,9 @@
+git init
+git config --global user.name "GonzaloPerezCadaveira"
+git config --global user.email "gperezmdp@gmail.com" 
+git remote add origin https://github.com/GonzaloPerezCadaveira/Prueba-node.git
+git remote -v
+git add .
+git "status"
+git commit -m
+git log
